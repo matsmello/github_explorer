35cfad58fc64c30acbb1c45ff63697f408a79e29
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-22 21:32:35 -0300

    Create basic navigation using BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <h1></h1>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
