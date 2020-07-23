2946fef7972716c0207dc8e1ae77bcf73b50313d
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-22 23:56:06 -0300

    Create style on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Title, Form } from "./styles";
+import { Title, Form, Repositories } from "./styles";
 import logoImage from "./../../assets/logo.svg";
+import { FiChevronRight } from "react-icons/fi";
 
 const Dashboard: React.FC = () => {
   return (
@@ -12,6 +13,18 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button>Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="test">
+          <img src="" alt="Nome" />
+          <div>
+            <strong>Titulo</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -22,6 +22,11 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
@@ -36,6 +41,58 @@ export const Form = styled.form`
 
     &:hover {
       background: ${shade(0.2, "#04d361")};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translate(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
