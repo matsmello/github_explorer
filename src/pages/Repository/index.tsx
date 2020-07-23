eedd322c103835967651d00cc831d344024edfb2
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-23 10:54:29 -0300

    Create Repository style'

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useRouteMatch, Link } from "react-router-dom";
+import { Header, RepositoryInfo } from "./styles";
+import logo from "./../../assets/logo.svg";
+import { FiChevronLeft } from "react-icons/fi";
+interface Repository {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<Repository>();
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header></header>
+      </RepositoryInfo>
+    </>
+  );
 };
 
 export default Repository;
