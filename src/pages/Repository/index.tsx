ae8ee140e63c2809fbe58a51575504a60a173a57
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-23 10:54:50 -0300

    Create Issues component

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useRouteMatch, Link } from "react-router-dom";
-import { Header, RepositoryInfo } from "./styles";
+import { Header, RepositoryInfo, Issues } from "./styles";
 import logo from "./../../assets/logo.svg";
-import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 interface Repository {
   repository: string;
 }
@@ -15,14 +15,45 @@ const Repository: React.FC = () => {
       <Header>
         <img src={logo} alt="Github Explorer" />
 
-        <Link to="/dashboard">
+        <Link to="/">
           <FiChevronLeft size={16} />
           Voltar
         </Link>
       </Header>
       <RepositoryInfo>
-        <header></header>
+        <header>
+          <img />
+          <div>
+            <strong></strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
       </RepositoryInfo>
+
+      <Issues>
+        <Link to={`repositories/${1}`}>
+          <div>
+            <strong>{1}</strong>
+            <p>{1}</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
