b52a0d597f2dbb4f1f61787fe57202bab76d0338
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-22 23:28:36 -0300

    Create base style on project

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Title, Form } from "./styles";
+import logoImage from "./../../assets/logo.svg";
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button>Pesquisar</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;
